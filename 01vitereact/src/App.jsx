a241d245d62ced7f42c1c392644b31dba0853d46
Himanshu Kumar <himanshuk1602@gmail.com>
2025-08-25 20:24:16 +0530

    first project in react

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,23 @@
+// we also need to import the file to run because this is the
+// main file by which our program is run
+
+import Anshu from "./anshu"
+
+function App() {
+
+
+  return (
+    <>
+     <Anshu/> // here we gain the code or file anshu 
+     <h1>Hello let's start learning React</h1>
+     <p> here we learn react with Hitesh</p>
+    </>
+  )
+}
+ 
+export default App
+
+
+// in jsx we return only ine element
+
+// but for many we use <> </>... called fragments
---
+++ 01vitereact/src/anshu.jsx
@@ -0,0 +1,19 @@
+// If you meant to render a React component, start its name with an uppercase letter
+// wothout upperCase this will not executed
+function Anshu (){
+    return (
+        <h3>Code is ready </h3>
+    )
+}
+
+export default Anshu
+
+// first we send this function or anshu in app.jsx
+
+/*anshu is a component 
+  every function name's first letter should be capital
+  take file name also in capital
+  when a component is returning html then the file name must be .jsx
+  .js and .jsx are same but .jsx is a good pratice in react
+  
+  */
